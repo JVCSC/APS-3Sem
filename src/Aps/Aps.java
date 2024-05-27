bb0b16debd82797321e40a7b1c8587a6749941bf
Joao <jvcontato99@gmail.com>
2024-05-27 16:29:08 -0300

    ajustando

--- src/Aps/Aps.java
+++ src/Aps/Aps.java
@@ -5,13 +5,10 @@ import aps.Controller.MetaESGController;
 
 public class Aps {
     public static void main(String[] args) {
-        // Cria a instância da View
         MetaESGView view = new MetaESGView();
         
-        // Cria a instância do Controller passando a View
         MetaESGController controller = new MetaESGController(view);
         
-        // Torna a View visível
         view.setVisible(true);
     }
 }
